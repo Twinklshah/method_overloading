f5c2dac450fb268639c3dce098f02ad3994c468c
Twinkle Shah <41550937+Twinklshah@users.noreply.github.com>
2022-07-15 17:33:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace method_overloading
+{
+    public class override_ex
+    {
+        public int Add(int a,int b)
+        {
+            int result1 = a + b;
+            return result1;
+        }
+         public int Add(int a,int b,int c)
+        {
+            int result = a + b+c;
+            return result;
+        }
+        
+    }
+   public  class Program
+    {
+        static void Main(string[] args)
+        {
+            override_ex e = new override_ex();
+            e.Add(23, 4);
+            e.Add(4, 5, 6);
+            // Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
